1e77337107157bbf4387507199d0194d4d36ccb4
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-05-09 19:33:12 -0600

    client side encryption

--- streams/stream-client.js
+++ streams/stream-client.js
@@ -1,30 +1,36 @@
-const { request }          = require('http');
-const { createGzip }       = require('zlib');
-const { createReadStream } = require('fs');
-const { basename }         = require('path');
+const { request } = require("http");
+const { createGzip } = require("zlib");
+const { createReadStream } = require("fs");
+const { basename } = require("path");
+const { createCipheriv, randomBytes } = require("crypto");
 
-const filename   = process.argv[2];
+const filename = process.argv[2];
 const serverHost = process.argv[3];
+const secret = process.argv[4];
+
+const initVector = randomBytes(16);
 
 const httpRequestOptions = {
   hostname: serverHost,
-  port:     3000,
-  path:     '/',
-  method:   'PUT',
-  headers:  {
-    'Content-Type':     'application/octet-stream',
-    'Content-Encoding': 'gzip',
-    'X-Filename':       basename( filename )
-  }
+  port: 3000,
+  path: "/",
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/octet-stream",
+    "Content-Encoding": "gzip",
+    "X-Filename": basename(filename),
+    "X-Initialization-Vector": initVector.toString("hex"),
+  },
 };
 
-const req = request( httpRequestOptions, ( res ) => {
-  console.log( `Server response: ${res.statusCode}` );
+const req = request(httpRequestOptions, (res) => {
+  console.log(`Server response: ${res.statusCode}`);
 });
 
-createReadStream( filename )
-  .pipe( createGzip() )
-  .pipe( req )
-  .on( 'finish', () => {
-    console.log( 'File sent succesfully' );
+createReadStream(filename)
+  .pipe(createGzip())
+  .pipe(createCipheriv("aes192", secret, initVector))
+  .pipe(req)
+  .on("finish", () => {
+    console.log("File sent succesfully");
   });
--- streams/stream-server.js
+++ streams/stream-server.js
@@ -1,23 +1,23 @@
-const { createServer }      = require('http');
-const { createGunzip }      = require('zlib');
-const { createWriteStream } = require('fs');
-const { basename, join }    = require('path');
+const { createServer } = require("http");
+const { createGunzip } = require("zlib");
+const { createWriteStream } = require("fs");
+const { basename, join } = require("path");
 
-const server = createServer( ( req, res ) => {
-  const filename     = basename( req.headers['x-filename'] );
-  const destFilename = join( 'received_files', filename );
+const server = createServer((req, res) => {
+  const filename = basename(req.headers["x-filename"]);
+  const destFilename = join("received_files", filename);
 
-  console.log( `File request received: ${filename}` );
+  console.log(`File request received: ${filename}`);
 
   req
-    .pipe( createGunzip() )
-    .pipe( createWriteStream(destFilename) )
-    .on( 'finish', () => {
-      res.writeHead( 201, { 'Content-Type': 'text/plain' } );
-      res.end( 'OK\n' );
-      
-      console.log( `File saved: ${destFilename}` );
+    .pipe(createGunzip())
+    .pipe(createWriteStream(destFilename))
+    .on("finish", () => {
+      res.writeHead(201, { "Content-Type": "text/plain" });
+      res.end("OK\n");
+
+      console.log(`File saved: ${destFilename}`);
     });
 });
 
-server.listen( 3000, () => console.log( 'Listening on port 3000') );
+server.listen(3000, () => console.log("Listening on port 3000"));
