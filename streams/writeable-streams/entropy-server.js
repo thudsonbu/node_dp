cf9db9fdcb7952a47dad9143ed1a8a30f7f502b9
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-05-09 21:34:01 -0600

    writeable stream with backpressure adjustment

---
+++ streams/writeable-streams/entropy-server.js
@@ -0,0 +1,35 @@
+const { createServer } = require("http");
+const Chance = require("chance");
+
+const chance = new Chance();
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  function generateMore() {
+    while (chance.bool({ likelihood: 95 })) {
+      const randomChunk = chance.string({
+        length: 16 * 1024 - 1,
+      });
+
+      // response object implements the writeable interface
+      const shouldContinue = res.write(`${randomChunk}\n`);
+
+      if (!shouldContinue) {
+        console.log("back-pressure");
+
+        return res.once("drain", generateMore);
+      }
+    }
+  }
+
+  res.end("\n\n");
+  res.on("finish", () => console.log("All data send"));
+});
+
+// register listener for finished event on socket
+server.listen(8080, () => {
+  console.log("Server listening on localhost:8080");
+});
+
+// use curl localhost:8080 to test the server (after starting it)
