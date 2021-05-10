4d27e55761d8468b0dfaf62b0fc8e3437c403879
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-05-09 20:23:00 -0600

    custom readable prototype and async iterator

---
+++ streams/readable-streams/async-iterator.js
@@ -0,0 +1,12 @@
+/**
+ * Reads strings from standard input and logs the input
+ */
+async function main() {
+  for await (const chunk of process.stdin) {
+    console.log("New data available");
+    console.log(`Chunk read (${chunk.length} bytes): ${chunk.toString()}`);
+  }
+  console.log("End of stream");
+}
+
+main();
