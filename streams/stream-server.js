0632bde62b1a0f6a760d5e852dec828602b38144
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-05-09 19:41:01 -0600

    server side decryption

--- streams/stream-server.js
+++ streams/stream-server.js
@@ -2,14 +2,20 @@ const { createServer } = require("http");
 const { createGunzip } = require("zlib");
 const { createWriteStream } = require("fs");
 const { basename, join } = require("path");
+const { createDecipheriv, randomBytes } = require("crypto");
+
+const secret = randomBytes(24);
+console.log(`Generated secret: ${secret.toString("hex")}`);
 
 const server = createServer((req, res) => {
   const filename = basename(req.headers["x-filename"]);
+  const initVector = Buffer.from(req.headers["x-initialization-vector"], "hex");
   const destFilename = join("received_files", filename);
 
   console.log(`File request received: ${filename}`);
 
   req
+    .pipe(createDecipheriv("aes192", secret, initVector))
     .pipe(createGunzip())
     .pipe(createWriteStream(destFilename))
     .on("finish", () => {
