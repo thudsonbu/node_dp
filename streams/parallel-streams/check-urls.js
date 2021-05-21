9f4df0585e89be0e50f49e478593f0ab2946c8bc
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-05-20 21:10:12 -0600

    url checker

--- streams/parallel-streams/check-urls.js
+++ streams/parallel-streams/check-urls.js
@@ -1,5 +1,34 @@
-const { createReadStream, createWriteStream } = require('fs');
-const { pipeline }                            = require('stream');
-const split                                   = require('split');
-const superagent                              = require('split');
-const { ParallelStream }                      = require('')
+const { createReadStream, createWriteStream } = require("fs");
+const { pipeline } = require("stream");
+const split = require("split");
+const superagent = require("superagent");
+const { ParallelStream } = require("./parallel-stream");
+
+pipeline(
+  createReadStream(process.argv[2]),
+  split(),
+
+  new ParallelStream(async (url, enc, push, done) => {
+    if (!url) {
+      return done();
+    }
+
+    try {
+      await superagent.head(url, { timeout: 5 * 1000 });
+      push(`${url} is up\n`);
+    } catch (error) {
+      push(`${url} is down\n`);
+    }
+  }),
+
+  createWriteStream("results.txt"),
+
+  (error) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+
+    console.log("All urls have been checked");
+  }
+);
