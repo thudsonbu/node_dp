606f8059a02cb59605a570052c4cda996579bd88
thudsonbu <thudson@starry.com>
2021-11-17 12:53:01 -0700

    Promises vs microtasts explanation

--- tricky-js/tasks-vs-microtasks.js
+++ tricky-js/tasks-vs-microtasks.js
@@ -7,9 +7,18 @@ function callback() {
   )
 }
 
+
 function then() {
+  count = 0;
+
   return Promise.resolve().then( () => {
     console.log( 'then' );
+
+    if ( count === 0 ) {
+      then();
+    }
+
+    count++;
   });
 }
 
@@ -21,14 +30,18 @@ function promise() {
 }
 
 function promiseCallback() {
+  let count = 0;
+
   return new Promise( ( resolve, reject ) => {
+
     setTimeout(
       () => {
         console.log( 'promiseCallback' );
         resolve();
       },
       0
-    )
+    );
+
   });
 }
 
@@ -48,3 +61,17 @@ sync();
 // then
 // callback
 // promiseCallback
+
+/**
+ * Explanation:
+ *
+ * The promise function creates a promise and in its constructor synchronously
+ * runs console.log() and resolves. The callback function creates a standard task
+ * and adds it to the queue. The promiseCallback function does the same. The then
+ * function executes the constructor and immediately resolves. Since this is a
+ * promise and not a callback, it creates a microtask. Microtasks are completed
+ * before tasks and when a new microtask created it does not wait till the next
+ * process tick, so it executes then once more before the callbacks. Finally
+ * in the order that they were qued the callbacks execute, callback, then
+ * promiseCallback.
+ */
