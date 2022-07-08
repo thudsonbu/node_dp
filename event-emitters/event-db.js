a357fdde127af311b016666a57e460b2b03bcb07
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-07-07 21:40:44 -0600

    feat(event-emitters:event-db): type definitions and insert many

--- event-emitters/event-db.js
+++ event-emitters/event-db.js
@@ -1,44 +1,157 @@
 const EventEmitter = require('events');
 const fs           = require('fs/promises');
+const path         = require('path');
 const crypto       = require('crypto');
 const { pipeline } = require('node:stream/promises');
-const Find         = require('./find');
+const Find         = require('./find-stream');
+
+/**
+ * Database document
+ * @typedef {Object} Document
+ * @property {string} id - id of document
+ * @property {string} data - data of document
+ */
+
+/**
+ * Insert event.
+ * @event EventDB#insert
+ * @type {Document}
+ */
+
+/**
+ * Find event.
+ * @event EventDB#find
+ * @type {Document}
+ */
 
 class EventDB extends EventEmitter {
-  constructor( name ) {
+  constructor( name, opts ) {
     super();
 
+    this.dataDelimiter     = opts?.dataDelimiter || '::::';
+    this.documentDelimiter = opts?.documentDelimiter || '\n';
+
+    if ( this.dataDelimiter === this.documentDelimiter ) {
+      throw new Error('dataDelimiter must not equal documentDelimiter');
+    }
+
     this.name      = name;
-    this.file_path = `./${ this.name }`;
+    this.file_path = path.resolve( __dirname, `./${ this.name }` );
   }
 
-  async find( id ) {
-    const find = new Find( id );
+  /**
+   * Find a document by its id
+   *
+   * @param {string} id - id to search with
+   *
+   * @emits EventDB#find
+   *
+   * @returns {Promise<Document>}
+   */
+  async findById( id ) {
+    return new Promise( ( resolve, reject ) => {
+      const find = new Find( id, this.dataDelimiter, this.documentDelimiter );
 
-    find.on( 'result', r => {
-      this.emit( 'result', r );
+      find.on( 'find', data => {
+        const returnValue = { id, data };
+
+        this.emit( 'find', returnValue );
+        resolve( returnValue );
+      });
+
+      find.on( 'not found', () => {
+        this.emit('not found');
+        reject('not not found');
+      });
+
+      pipeline(
+        fs.readFile( this.file_path, { encoding: 'utf-8' } ),
+        find
+      );
     });
-
-    await pipeline(
-      fs.readFile( this.file_path, { encoding: 'utf-8' } ),
-      find
-    );
   }
 
+  /**
+   * Insert a document into the db
+   *
+   * @param {string} data - data to be inserted
+   *
+   * @emits EventDB#insert
+   *
+   * @returns {Promise<Document>}
+   */
   async insert( data ) {
-    const id           = crypto.randomUUID();
-    const data_with_id = `${ id }:::${ data }:::`;
+    const { documentString, id } = this.createDocumentString( data );
 
     await fs.writeFile(
       this.file_path,
-      data_with_id,
+      documentString,
       { encoding: 'utf-8', flag: 'a' }
     );
 
-    this.emit( id, data );
+    const returnValue = { id, data };
 
-    return { id, data };
+    this.emit( 'insert', returnValue );
+
+    return returnValue;
   };
+
+  /**
+   * Insert multiple documents into the db
+   *
+   * @param {string[]} data - data to be inserted
+   *
+   * @typedef {Object} InsertManyResult
+   * @property {Document[]} results - insert results in order
+   * @property {number} count - number of items inserted
+   *
+   * @returns {Promise<InsertManyResult>}
+   */
+  async insertMany( data ) {
+    let blob = '';
+
+    const results = data.map( d => {
+      const { documentString, id, data } = this.createDocumentString( d );
+
+      blob += documentString;
+
+      return { id, data };
+    });
+
+    await fs.writeFile(
+      this.file_path,
+      blob,
+      { encoding: 'utf-8', flag: 'a' }
+    );
+
+    const returnValue = { results, count: results.length };
+
+    this.emit( 'insertMany', returnValue );
+
+    return returnValue;
+  }
+
+  /**
+   * Create a valid document string for storage in db
+   *
+   * @param {string} data - data of document to be created
+   *
+   * @typedef {Object} CreateDocumentStringResult
+   * @property {string} documentString- document valid document string
+   * @property {string} id - id in documentString
+   * @property {string} data - data in documentString
+   *
+   * @returns {CreateDocumentStringResult}
+   */
+  createDocumentString( data ) {
+    const id = crypto.randomUUID();
+
+    return {
+      documentString: `${ id }${ this.dataDelimiter }${ data }${ this.documentDelimiter }`, // eslint-disable-line max-len
+      id,
+      data
+    };
+  }
 };
 
 module.exports = EventDB;
