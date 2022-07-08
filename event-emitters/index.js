a8aa054066ae7675db50d8181f56a915ca7cb437
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-07-07 21:42:08 -0600

    feat(event-emitters:event-db): testing

--- event-emitters/index.js
+++ event-emitters/index.js
@@ -1,13 +1,25 @@
-const EventDB = require('./event-db');
+const EventDB   = require('./event-db');
+const { faker } = require('@faker-js/faker');
 
 async function main() {
   const db = new EventDB('names');
 
-  db.on( 'result', d => console.log( d ) );
+  const names = Array.from({ length: 5000 }).map( () => {
+    return faker.name.firstName();
+  });
 
-  const { id, data } = await db.insert('tom');
+  console.time('insertMany');
+  const { results } = await db.insertMany( names );
+  console.timeEnd('insertMany');
 
-  await db.find( id );
+  const randomDocument = results[ Math.floor( Math.random() * 5000 ) ];
+
+  console.time('find');
+  const found = await db.findById( randomDocument.id );
+  console.timeEnd('find');
+
+  console.log( 'found', found );
+  console.log( 'actual', randomDocument );
 }
 
 main().then();
